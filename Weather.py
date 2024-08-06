52d59a11d607fcba28cf5dd47d859596387b022a
cupofteame <malwarexxx@protonmail.com>
2024-08-05 19:05:53 -0500

    api change openweather

--- Weather.py
+++ Weather.py
@@ -8,7 +8,7 @@ load_dotenv()
 app = Flask(__name__)
 
 API_KEY = os.getenv('API_KEY')  # Get API key from environment variable
-API_URL = 'http://api.weatherapi.com/v1/current.json'
+API_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,9 +19,24 @@ def index():
         city = request.form.get('city')
         if city:
             try:
-                response = requests.get(f"{API_URL}?key={API_KEY}&q={city}")
-                response.raise_for_status()
-                weather_data = response.json()
+                response_metric = requests.get(f"{API_URL}?q={city}&appid={API_KEY}&units=metric")
+                response_metric.raise_for_status()
+                weather_data_metric = response_metric.json()
+
+                response_imperial = requests.get(f"{API_URL}?q={city}&appid={API_KEY}&units=imperial")
+                response_imperial.raise_for_status()
+                weather_data_imperial = response_imperial.json()
+
+                weather_data = {
+                    'city': weather_data_metric['name'],
+                    'country': weather_data_metric['sys']['country'],
+                    'temp_celsius': weather_data_metric['main']['temp'],
+                    'temp_fahrenheit': weather_data_imperial['main']['temp'],
+                    'description': weather_data_metric['weather'][0]['description'],
+                    'icon': weather_data_metric['weather'][0]['icon'],
+                    'humidity': weather_data_metric['main']['humidity'],
+                    'wind_speed': weather_data_metric['wind']['speed']
+                }
             except requests.RequestException as e:
                 error = f"An error occurred: {str(e)}"
 
